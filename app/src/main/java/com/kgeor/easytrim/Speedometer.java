a9394208b86eb41a1a82afa666f12dd6755bacec
Keegan George <kgeorge13@gmail.com>
2018-07-23 20:26:50 -0700

    Created a fragment for viewing the speedometer

---
+++ app/src/main/java/com/kgeor/easytrim/Speedometer.java
@@ -0,0 +1,34 @@
+package com.kgeor.easytrim;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Speedometer extends Fragment {
+
+
+    public Speedometer() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_speedometer, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
