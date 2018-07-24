5b259551dfa9376b8459719209ed7002fd99de03
Keegan George <kgeorge13@gmail.com>
2018-07-23 22:29:03 -0700

    Fixed bug where trim value was not being set correctly
    Fixed this bug:
    "When opening for first time and going straight to calibration view  (without ever clicking on TrimView yet) and when "setting trim" the Trim Value is always stored in the database as '0' . This is probably because the rotation sensor is not being implemented yet "
    
    Also, removed TODOS

--- app/src/main/java/com/kgeor/easytrim/CalibrationTempActivity.java
+++ app/src/main/java/com/kgeor/easytrim/CalibrationTempActivity.java
@@ -1,5 +1,10 @@
 package com.kgeor.easytrim;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +14,16 @@ import android.widget.Toast;
 import static com.kgeor.easytrim.MainActivity.boatTrim;
 import static com.kgeor.easytrim.Speedometer.currentSpeedValue;
 
-public class CalibrationTempActivity extends AppCompatActivity implements DataCommunication, View.OnClickListener {
-    // TODO add functionality to start calculating the trim values
+public class CalibrationTempActivity extends AppCompatActivity implements SensorEventListener, DataCommunication, View.OnClickListener {
     private Button submitTrim;
     static MyDatabase db;
+
+    // SENSOR INFORMATION FOR DETECTING TRIM //
+    private static final int SENSOR_DELAY_MICROS = 16 * 1000; // 16 ms
+    private SensorManager mSensorManager;
+    @Nullable
+    private Sensor mRotationSensor;
+
 
 
     @Override
@@ -21,12 +32,36 @@ public class CalibrationTempActivity extends AppCompatActivity implements DataCo
         setContentView(R.layout.activity_calibration_temp);
         submitTrim = findViewById(R.id.set_trim);
 
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        // can be null if the sensor hardware is not available
+        mRotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
 
         submitTrim.setOnClickListener(this);
 
         db = new MyDatabase(this);
 
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mRotationSensor == null) {
+            Toast.makeText(this, "Rotation Sensor Unavailable", Toast.LENGTH_LONG).show();
+        }
+
+        mSensorManager.registerListener(this, mRotationSensor, SENSOR_DELAY_MICROS);
+
+    }
+
 
 
     @Override
@@ -35,15 +70,6 @@ public class CalibrationTempActivity extends AppCompatActivity implements DataCo
     }
 
     public void addBoatSpecs() {
-        // TODO FIX BUG! EXPLAINED BELOW:
-        /*
-         * When opening for first time and going straight to calibration view
-         * (without ever clicking on TrimView yet) and when "setting trim"
-         * the Trim Value is always stored in the database as '0'
-         *
-         * This is probably because the rotation sensor
-         * is not being implemented yet
-         */
         long id = db.insertData(currentSpeedValue, (int) boatTrim);
 
         if (id < 0) {
@@ -61,6 +87,33 @@ public class CalibrationTempActivity extends AppCompatActivity implements DataCo
                 addBoatSpecs();
                 break;
         }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == mRotationSensor) {
+            updateOrientation(event.values);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO Auto-generated method stub
+    }
+
+    @SuppressWarnings("SuspiciousNameCombination")
+    private void updateOrientation(float[] rotationVector) {
+        float[] rotationMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, rotationVector);
+
+        // Transform rotation matrix into azimuth/pitch/roll (pitch == boatTrim)
+        float[] orientation = new float[3];
+        SensorManager.getOrientation(rotationMatrix, orientation);
+
+        // Convert radians to degrees
+        boatTrim = orientation[1] * -57.295779513f;
+
+
     }
 
 //    public void addBoatSpecs(View view) {
