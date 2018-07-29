61699139a7fea3f159fbad6ed65eccd15410e007
Keegan George <kgeorge13@gmail.com>
2018-07-28 21:28:09 -0700

    Created Tutorial Activity encapsulating Onboarding Stepper

---
+++ app/src/main/java/com/kgeor/easytrim/Tutorial.java
@@ -0,0 +1,44 @@
+package com.kgeor.easytrim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.stepstone.stepper.StepperLayout;
+import com.stepstone.stepper.VerificationError;
+
+public class Tutorial extends AppCompatActivity implements StepperLayout.StepperListener {
+    private StepperLayout mStepperLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+
+        mStepperLayout = findViewById(R.id.stepper_layout);
+        mStepperLayout.setAdapter(new MyStepperAdapter(getSupportFragmentManager(), this));
+        mStepperLayout.setListener(this);
+
+    }
+
+    @Override
+    public void onCompleted(View completeButton) {
+        Toast.makeText(this, "onCompleted!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onError(VerificationError verificationError) {
+        Toast.makeText(this, "onError! -> " + verificationError.getErrorMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStepSelected(int newStepPosition) {
+        Toast.makeText(this, "onStepSelected! -> " + newStepPosition, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onReturn() {
+        finish();
+    }
+}
