7c37a7376e2f7b532ab230b0f3990cb837a9709d
Keegan George <kgeorge13@gmail.com>
2018-07-28 21:27:35 -0700

    Created Adapter for onboarding stepper screen

---
+++ app/src/main/java/com/kgeor/easytrim/MyStepperAdapter.java
@@ -0,0 +1,65 @@
+package com.kgeor.easytrim;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+
+import com.kgeor.easytrim.R;
+import com.kgeor.easytrim.StepFragmentWelcome;
+import com.stepstone.stepper.Step;
+import com.stepstone.stepper.adapter.AbstractFragmentStepAdapter;
+import com.stepstone.stepper.viewmodel.StepViewModel;
+
+public class MyStepperAdapter extends AbstractFragmentStepAdapter {
+    private static final String CURRENT_STEP_POSITION_KEY = "messageResourceId";
+
+
+    public MyStepperAdapter(@NonNull FragmentManager fm, @NonNull Context context) {
+        super(fm, context);
+    }
+
+    @Override
+    public Step createStep(int position) {
+        switch (position){
+            case 0:
+                final StepFragmentWelcome step1 = new StepFragmentWelcome();
+                Bundle b1 = new Bundle();
+                b1.putInt(CURRENT_STEP_POSITION_KEY, position);
+                step1.setArguments(b1);
+                return step1;
+            case 1:
+                final StepFragmentBoatDetails step2 = new StepFragmentBoatDetails();
+                Bundle b2 = new Bundle();
+                b2.putInt(CURRENT_STEP_POSITION_KEY, position);
+                step2.setArguments(b2);
+                return step2;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+
+    @NonNull
+    @Override
+    public StepViewModel getViewModel(@IntRange(from = 0) int position) {
+        //Override this method to set Step title for the Tabs, not necessary for other stepper types
+        switch (position){
+            case 0:
+                return new StepViewModel.Builder(context)
+                        .setTitle("Tabs 1") //can be a CharSequence instead
+                        .create();
+            case 1:
+                return new StepViewModel.Builder(context)
+                        .setTitle("Tabs 2") //can be a CharSequence instead
+                        .create();
+        }
+        return null;
+
+    }
+}
