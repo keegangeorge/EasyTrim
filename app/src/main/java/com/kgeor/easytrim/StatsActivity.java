4369118b0e9791c5281e29ef6069cf6c90915412
Keegan George <kgeorge13@gmail.com>
2018-07-31 07:33:05 -0700

    Added settings overflow menu to activity

--- app/src/main/java/com/kgeor/easytrim/StatsActivity.java
+++ app/src/main/java/com/kgeor/easytrim/StatsActivity.java
@@ -1,8 +1,12 @@
 package com.kgeor.easytrim;
 
 import android.content.Intent;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -40,5 +44,35 @@ public class StatsActivity extends AppCompatActivity implements View.OnClickList
         // OPEN TRIM DATA RESULTS UPON BUTTON PRESS //
         Intent i = new Intent(StatsActivity.this, TrimDataResults.class);
         startActivity(i);
+    }
+
+    /**
+     * Method responsible for what occurs when the settings items are clicked.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings_item:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, SettingsActivity.GeneralPreferenceFragment.class.getName());
+                intent.putExtra(PreferenceActivity.EXTRA_NO_HEADERS, true);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Method responsible for inflating the settings menu
+     *
+     * @param menu the settings menu to be inflated
+     * @return true if the menu is inflated
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflate = getMenuInflater();
+        inflate.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
