85059bbc6ff6143095495417bd61077062f168c7
Keegan George <kgeorge13@gmail.com>
2018-07-30 22:26:08 -0700

    Updated stat activity to include information

--- app/src/main/java/com/kgeor/easytrim/StatsActivity.java
+++ app/src/main/java/com/kgeor/easytrim/StatsActivity.java
@@ -1,13 +1,39 @@
 package com.kgeor.easytrim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class StatsActivity extends AppCompatActivity {
+public class StatsActivity extends AppCompatActivity implements View.OnClickListener {
+    // GUI //
+    TextView topSpeed, measurementUnit, condition;
+    Button viewData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        // LINK XML TO JAVA //
+        topSpeed = findViewById(R.id.top_speed);
+        measurementUnit = findViewById(R.id.unit_stat);
+        condition = findViewById(R.id.condition_stat);
+        viewData = findViewById(R.id.btnViewDataStat);
+
+        // SET LISTENERS //
+        viewData.setOnClickListener(this);
+
+        topSpeed.setText(Integer.toString(Speedometer.topSpeed) + " " + Speedometer.curUnits);
+        measurementUnit.setText(Speedometer.curUnits);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(StatsActivity.this, TrimDataResults.class);
+        startActivity(i);
     }
 }
