be09b72dc18542f8f78d1f8ff4db32378ba67aa1
Keegan George <kgeorge13@gmail.com>
2018-07-30 21:11:10 -0700

    Removed unnecessary parts of MainActivity

--- app/src/main/java/com/kgeor/easytrim/MainActivity.java
+++ app/src/main/java/com/kgeor/easytrim/MainActivity.java
@@ -31,42 +31,25 @@ import java.text.NumberFormat;
  * @author Keegan George
  * @version 1.0
  */
-public class MainActivity extends AppCompatActivity implements DataCommunication, SensorEventListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements DataCommunication, SensorEventListener {
     // FIELDS //
-//    public SpeedTask speedTask = new SpeedTask();
     LottieAnimationView trimCorrectAnimation;
 
 
     // GUI //
-//    private Button button;
-    //    private Button calibrateButton;
-    private Button submitTrim;
-    //    protected Button btnSpeed;
-    private TextView textView, trimTextView, trimStat, tempInfo;
     protected static DashboardView speedGauge;
 
     // SENSORS & GPS //
-//    private LocationManager locationManager;
-//    private LocationListener locationListener;
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int SENSOR_DELAY_MICROS = 16 * 1000; // 16 ms
     protected static float boatTrim = 0;
-//    protected static int finalSpeed = 0;
 
     private SensorManager mSensorManager;
     @Nullable
     private Sensor mRotationSensor;
-    float multiplier;
 
     // DATABASE //
     static MyDatabase db;
-
-    // SHARED PREFERENCES //
-//    private String unitsPref;
-//    private SharedPreferences sharedPref;
-
-    // UNITS //
-//    private String curUnits = "knots";
 
 
     @Override
@@ -97,27 +80,9 @@ public class MainActivity extends AppCompatActivity implements DataCommunication
         Log.i(TAG, "onCreate: ");
         WindowManager mWindowManager = this.getWindow().getWindowManager();
         // GUI REFERENCES //
-//        button = findViewById(R.id.button);
         trimCorrectAnimation = findViewById(R.id.trim_correct_animation);
-        tempInfo = findViewById(R.id.information);
         trimCorrectAnimation.setImageAssetsFolder("images/");
-//        btnSpeed = findViewById(R.id.btnSpeed);
-//        calibrateButton = findViewById(R.id.calibrate_button);
-//        submitTrim = findViewById(R.id.submit_trim);
-//        textView = findViewById(R.id.textView);
-        trimTextView = findViewById(R.id.pitch);
         speedGauge = findViewById(R.id.speed_gauge);
-        trimStat = findViewById(R.id.trim_up_or_down);
-
-//        calibrateButton.setOnClickListener(this);
-//        submitTrim.setOnClickListener(this);
-//        btnSpeed.setOnClickListener(this);
-
-        // SETTINGS //
-        // default values set in XML, this ensures SharedPreferences is initialized with default values
-//        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
-//        detectUnits();
-
 
         // DATABASE //
         db = new MyDatabase(this);
@@ -127,39 +92,12 @@ public class MainActivity extends AppCompatActivity implements DataCommunication
 
         // can be null if the sensor hardware is not available
         mRotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-
-//        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-
-        // PERMISSIONS //
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                /*
-//                 * Occurs on first launch of application and permissions are asked
-//                 */
-//                Log.i(TAG, "HERE_3");
-//                requestPermissions(new String[]{
-//                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
-//                        Manifest.permission.INTERNET
-//                }, 10);
-//                return;
-//            }
-//        }
-
-
-    } // onCreate() method end
-
-//    public void startSpeedCalc(View view) {
-//        SpeedTask speedTask = new SpeedTask();
-//        speedTask.execute();
-//    }
-
+    }
 
     @Override
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
-
     }
 
 
@@ -167,7 +105,6 @@ public class MainActivity extends AppCompatActivity implements DataCommunication
     protected void onResume() {
         Log.i(TAG, "onResume: ");
         super.onResume();
-//        detectUnits();
 
         if (mRotationSensor == null) {
             Toast.makeText(this, "Rotation Sensor Unavailable", Toast.LENGTH_LONG).show();
@@ -184,10 +121,6 @@ public class MainActivity extends AppCompatActivity implements DataCommunication
         if (event.sensor == mRotationSensor) {
             updateOrientation(event.values);
         }
-
-        NumberFormat numberFormat = NumberFormat.getNumberInstance();
-        numberFormat.setMaximumFractionDigits(0);
-        trimTextView.setText("CURRENT TRIM: " + (int) boatTrim);
     }
 
     @Override
@@ -210,83 +143,29 @@ public class MainActivity extends AppCompatActivity implements DataCommunication
 
     }
 
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-
-        }
-    }
-
-
-    public void viewDatabaseResults(View view) {
-        String data = db.getData();
-        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
-    }
-
-
     @Override
     public void viewQueryResults(int speed) {
         int convertedTrim = (int) boatTrim;
         int queryResults = db.getCloseToData(speed);
-
         int trimResults = db.getTrimData(speed);
-        System.out.println("Current Trim: " + boatTrim);
-        System.out.println("Trim for Speed: " + trimResults);
-        System.out.println("Database Speed: " + queryResults);
-        System.out.println("Actual Speed: " + speed);
-        tempInfo.setText("TRIM: " + convertedTrim + " TRIM FOR SPEED: " +
-                trimResults + " DB SPEED: " + queryResults + " REAL SPEED: " +
-                speed);
-
-//        if (queryResults == speed) {
-//            System.out.println("Boat's Actual Trim: " + convertedTrim);
-//            System.out.println("Boat's Desired Trim: " + trimResults);
-//            if (convertedTrim > trimResults) {
-//                trimCorrectAnimation.setAnimation("trim-down.json");
-//                trimCorrectAnimation.playAnimation();
-//                trimStat.setText("Need less trim!");
-//            } else if (convertedTrim < trimResults) {
-//                trimCorrectAnimation.setAnimation("trim-up.json");
-//                trimCorrectAnimation.playAnimation();
-//                trimStat.setText("Need more trim!");
-//
-//            } else if (convertedTrim == trimResults) {
-//                trimStat.setText("Trim is correct!");
-//                trimCorrectAnimation.setAnimation("steady.json");
-//                trimCorrectAnimation.playAnimation();
-//            }
-//        } else if(queryResults > speed) {
-//            trimStat.setText("DB GREATER THAT REAL");
-//        } else if (queryResults < speed) {
-//            trimStat.setText("DB LESS THAN REAL");
-//        } else {
-//            trimCorrectAnimation.pauseAnimation();
-//            trimStat.setText("Need calibration");
-//        }
+        // System.out.println("Current Trim: " + boatTrim);
+        // System.out.println("Trim for Speed: " + trimResults);
+        // System.out.println("Database Speed: " + queryResults);
+        // System.out.println("Actual Speed: " + speed);
 
         if (convertedTrim > trimResults) {
-//            trimCorrectAnimation.setAnimation("trim-down.json");
             trimCorrectAnimation.setAnimation("DECREASE_TRIM.json");
             trimCorrectAnimation.playAnimation();
-            trimStat.setText("Need less trim!");
         } else if (convertedTrim < trimResults) {
             trimCorrectAnimation.setAnimation("INCREASE_TRIM.json");
             trimCorrectAnimation.playAnimation();
-            trimStat.setText("Need more trim!");
-
         } else if (convertedTrim == trimResults) {
-            trimStat.setText("Trim is correct!");
             trimCorrectAnimation.setAnimation("NORMAL_TRIM.json");
             trimCorrectAnimation.playAnimation();
         } else {
             trimCorrectAnimation.pauseAnimation();
         }
-
-
     }
-
-
 } // MainActivity class end
 
 
